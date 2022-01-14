ed2c2219555d23db82f90199a0a5e5c0a82bf202
Matt Walters <mwalters8@gmail.com>
2022-01-14 14:55:36 -0500

    Add custom css file that will not be modified in releases

--- dev.sh
+++ dev.sh
@@ -7,7 +7,7 @@ cd src
 export FLASK_APP=homed
 export FLASK_ENV=development
 
-flask run
+flask run -p 5050
 
 # clean:
 #     docker ps -a | awk '{ print $1,$2 }' | grep mwalters/homed | awk '{print $1 }' | xargs -I {} docker rm {}
